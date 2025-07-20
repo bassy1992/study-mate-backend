bc96c186d9d713e60aa6e1333e9c9ac6cd1c362b
bassy1992 <125444070+bassy1992@users.noreply.github.com>
2025-07-20 06:02:47 -0500

    Delete frontend directory

--- frontend/clear-auth.js
+++
@@ -1,5 +0,0 @@
-// Run this in browser console to clear authentication
-console.log('Clearing authentication data...');
-localStorage.removeItem('authToken');
-sessionStorage.clear();
-console.log('Authentication cleared. Please refresh and login again.');
